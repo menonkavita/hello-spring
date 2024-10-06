39d34e818a513269acd6fea7f8c4dd458ce80b91
bluzL <menon_kavita@yahoo.com>
2024-10-06 16:19:42 -0500

    Thymeleaf - Iterating through template - Dynamic Forms

--- src/main/java/org/launchcode/hellospring/controllers/HelloControl.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloControl.java
@@ -2,7 +2,11 @@ package org.launchcode.hellospring.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloControl {
@@ -22,41 +26,59 @@ public class HelloControl {
     }
 
 
-    // Handles request of the form /hell?name=LaunchCode
-    @GetMapping("hello")
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name){
-        return "Hello " + name + " !";
+    // Handles request of the form /hello?name=LaunchCode   --- Not working ---
+    // Handling the form request using GET & POST
+    // Remove @ResponseBody when using Thymeleaf template to let browser know not to send response back
+    @RequestMapping(value="hello", method = {RequestMethod.GET, RequestMethod.POST})
+    public String helloWithQueryParam(@RequestParam String name, Model model){
+        String theGreeting = "Hello, " + name + " !";
+
+        // variable "theGreeting" matches the string variable of this function
+        // while "greeting" matches the variable in the hello.html file
+        model.addAttribute("greeting", theGreeting);
+
+        return "hello";
     }
 
     // Handles requests of the form /hello/LaunchCode
+    // Remove @ResponseBody when using Thymeleaf template to let browser know not to send reponse back
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloWithPathParam(@PathVariable String name){
-        return "Hello, " + name + " !";
+    public String helloWithPathParam(@PathVariable String name, Model model){
+       model.addAttribute("greeting", "Hello using Path Variable, " + name + " !");
+
+       // returns name of the html file that will handle
+       // if there was a subfolder in "templates" directory, then the return statement
+       // would say - return "subfolder/fileName";
+        return "hello";
     }
 
 
-    // Handles request of the form
+    // Handles request of the form localhost:8080/form
+    // Using resources/templates/form.html
     @GetMapping("form")
-    @ResponseBody
     public String helloForm(){
-        return "<html>" +
-                "<body>" +
-                "<form action='hello' method='post'>" +                   // submit a request to /hello
-                "<input type='text' name='name'>" +
-                "<input type='submit' value='Greet me!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+        return "form";
     }
 
+    // Handles requests @ /hello-names
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> theNames = new ArrayList<>();
+        theNames.add("Java");
+        theNames.add("LaunchCode");
+        theNames.add("JavaScript");
 
-    // Handling request of form using GET & POST
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value="hello")
-    @ResponseBody
-    public String helloGoodbyeWithQueryParam(@RequestParam String name){
-        return "Hello, " + name + "! And... Goodbye!!";
+        List<String> coffeeList = new ArrayList<>();
+        coffeeList.add("French Roast");
+        coffeeList.add("Espresso");
+        coffeeList.add("Kopi Luwak");
+        coffeeList.add("Instant");
+
+
+        model.addAttribute("names", theNames);
+        model.addAttribute("coffeeOptions", coffeeList);
+
+        return "hello-list";            // returns name of the html file that will handle
     }
 
 }
